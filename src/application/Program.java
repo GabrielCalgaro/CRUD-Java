991cae46e3ac27d196a7255aa79427fd528bc013
Gabriel Calgaro <calgaro.gabriel@gmail.com>
2020-01-10 12:22:51 -0400

    Classes Product e Category implementadas

--- src/application/Program.java
+++ src/application/Program.java
@@ -3,13 +3,17 @@ package application;
 import java.sql.Connection;
 
 import db.DB;
+import model.entities.Category;
+import model.entities.Product;
 
 public class Program {
 
 	public static void main(String[] args) {
 		
-		Connection conn = DB.getConnection();
-		DB.closeConnection();
+		Category obj = new Category(1,"book");
+		
+		Product product = new Product(4, "lotr", 10, 50.0, obj);
+		System.out.println(product);
 	}
 
 }
